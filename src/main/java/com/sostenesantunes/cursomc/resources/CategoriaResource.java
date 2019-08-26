a79bf1f37084313978abb861b72cc880bfce56ff
Sóstenes Antunes <sostenesantunes@gmail.com>
2019-08-26 09:42:43 -0300

    Testando a primeira classe de Domain

--- src/main/java/com/sostenesantunes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sostenesantunes/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.sostenesantunes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sostenesantunes.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
+	Categoria categoria  = new Categoria();
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String start() {
-		return "Aplicação Rest funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }

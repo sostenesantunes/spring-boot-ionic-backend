503e75d065e373cdb2189399ac98da08dace1cdd
Sóstenes Antunes <sostenesantunes@gmail.com>
2019-08-27 19:51:46 -0300

    Insersão Entity Cidade e Estado

--- src/main/java/com/sostenesantunes/cursomc/CursomcApplication.java
+++ src/main/java/com/sostenesantunes/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sostenesantunes.cursomc.domain.Categoria;
+import com.sostenesantunes.cursomc.domain.Cidade;
+import com.sostenesantunes.cursomc.domain.Estado;
 import com.sostenesantunes.cursomc.domain.Produto;
 import com.sostenesantunes.cursomc.repositories.CategoriaRepository;
+import com.sostenesantunes.cursomc.repositories.CidadeRepository;
+import com.sostenesantunes.cursomc.repositories.EstadoRepository;
 import com.sostenesantunes.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +21,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,10 +52,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,  c2, c3));
+		
 	}
 	
-	
-	
-	
-
 }

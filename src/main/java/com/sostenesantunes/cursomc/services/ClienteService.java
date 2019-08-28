6f26a2e0eaacd0cd6e1dd13aa8d6378f616b55a0
Sóstenes Antunes <sostenesantunes@gmail.com>
2019-08-28 20:20:41 -0300

    EndPoint /Clinetes/{id} disponível

---
+++ src/main/java/com/sostenesantunes/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.sostenesantunes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sostenesantunes.cursomc.domain.Cliente;
+import com.sostenesantunes.cursomc.repositories.ClienteRepository;
+import com.sostenesantunes.cursomc.services.exceptions.ObjectNotfoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository catRepos;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = catRepos.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotfoundException(
+					"Objeto não encontrado Id: " + id  +", Tipo: " + Cliente.class.getName()));
+		
+		
+	}
+}

8184bbf612a165b34dad60fb331169788d58aa85
Sóstenes Antunes <sostenesantunes@gmail.com>
2019-09-06 20:10:41 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/sostenesantunes/cursomc/domain/Cidade.java
+++ src/main/java/com/sostenesantunes/cursomc/domain/Cidade.java
@@ -2,15 +2,12 @@ package com.sostenesantunes.cursomc.domain;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cidade implements Serializable {
@@ -21,7 +18,6 @@ public class Cidade implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "estado_id")
 	private Estado estado;

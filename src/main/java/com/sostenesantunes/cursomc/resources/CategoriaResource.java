0b9ec6553b4e3cb939397ba948e32081492f82fc
Sóstenes Antunes <sostenesantunes@gmail.com>
2019-09-09 18:23:44 -0300

    Inserindo uma nova categoria com method POST

--- src/main/java/com/sostenesantunes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sostenesantunes/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,15 @@
 package com.sostenesantunes.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sostenesantunes.cursomc.domain.Categoria;
 import com.sostenesantunes.cursomc.services.CategoriaService;
@@ -26,5 +26,14 @@ public class CategoriaResource {
 		
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }

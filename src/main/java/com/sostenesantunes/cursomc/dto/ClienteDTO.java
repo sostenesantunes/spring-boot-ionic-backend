acf7615289db2cb2d734700bf71be211d5b09e08
Sóstenes Antunes <sostenesantunes@gmail.com>
2019-09-23 20:57:20 -0300

    PUT, DELETE, E GET para Cliente

---
+++ src/main/java/com/sostenesantunes/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package com.sostenesantunes.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sostenesantunes.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho do nome deve ter entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email Inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
--- src/main/java/com/sostenesantunes/cursomc/services/ClienteService.java
+++ src/main/java/com/sostenesantunes/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.sostenesantunes.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sostenesantunes.cursomc.domain.Cliente;
+import com.sostenesantunes.cursomc.dto.ClienteDTO;
 import com.sostenesantunes.cursomc.repositories.ClienteRepository;
+import com.sostenesantunes.cursomc.services.exceptions.DataIntegrityViolation;
 import com.sostenesantunes.cursomc.services.exceptions.ObjectNotfoundException;
 
 @Service
@@ -18,7 +25,41 @@ public class ClienteService {
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = catRepos.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotfoundException(
-					"Objeto não encontrado Id: " + id  +", Tipo: " + Cliente.class.getName()));	
-		
+					"Objeto não encontrado Id: " + id  +", Tipo: " + Cliente.class.getName()));		
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return catRepos.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			catRepos.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolation(
+					"Não é possível excluir porque há Entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return catRepos.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest	 = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return catRepos.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto){
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	protected void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 }
